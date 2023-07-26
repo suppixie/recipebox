bdefc9176764b35ce0a422698163bb841d19b314
suppixie <mamthapatalay6@gmail.com>
2023-07-27 02:33:10 +0530

    made changes to the instructions block

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -1,53 +1,51 @@
 import React,{useState,useEffect} from "react";
 import { Configuration, OpenAIApi } from "openai";
-const axios = require('axios');
 
 
 function Instructions(props){
-    const [result, setResult] = useState([]);
-    const prompt = `give me the instructions to cook, in bullets without displaying ingredients again-
-        recipe: ${props.label}
-        ingredients:${props.ingredientLines}`;
+    const configuration = new Configuration({
 
-    function getInstructions() {
-            const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
-            const apiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
+        apiKey: process.env.REACT_APP_OPENAI_API_KEY
+     });
+    //  console.log(process.env.REACT_APP_OPENAI_API_KEY);
 
-            const headers = {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${apiKey}`
-            };
-            
-            const data = {
-            model: "text-davinci-003",
-            prompt: prompt,
-            max_tokens: 10,
-            temperature: 0
-            };
-
-            axios.post(apiUrl, data, { headers })
-            .then(response => {
-                const instructionDetails = response.data.choices[0].text;
-                let sentences = instructionDetails.split(/(?<![a-zA-Z\d])\d+. /);
-                sentences = sentences.filter(element => isNaN(element));
-                console.log(sentences);
-                setResult(sentences);
-            })
-            .catch(error => {
-                console.error("Error:", error.response.data); // Handle errors
-            });
-        }
-
-        useEffect(()=>{
-            getInstructions();
-        },[prompt]);
+     const openai = new OpenAIApi(configuration);
+   
+     const [result, setResult] = useState([]);
+    
+     const prompt = `give me the instructions to cook, in bullets without displaying ingredients again-
+     recipe: ${props.label}
+     ingredients:${props.ingredientLines}`;
+   
+     async function generateInstructions() {
+       const response = await openai.createCompletion({
+         model: "text-davinci-003",
+         prompt: prompt,
+         temperature: 0,
+         max_tokens: 1500,
+         frequency_penalty: 0,
+         presence_penalty: 0,
+       });
+   
+       const instructionDetails = response.data.choices[0].text
+       let sentences = instructionDetails.match(/(?<=• ).+?(?=\n|$)/g);
+    //    sentences = sentences.filter(element => isNaN(element));
+       console.log(sentences);
+       setResult(sentences);
+         }
+     useEffect(() => {
+       generateInstructions();
+     }, [prompt]);
 
         return(
             <div className="instructions">
                  {result.map((instruction,index)=>{
                     return(
-                        <div key={index}>
-                            {instruction}
+                        <div>
+                            <h3>Instructions</h3>
+                            <ul>
+                                <li key={index}>{instruction}</li>
+                            </ul>
                         </div>
                     )
                 })}
