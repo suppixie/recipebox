e182de2c80e6143c3a6321a8b8843c89d5a89111
suppixie <mamthapatalay6@gmail.com>
2023-07-28 04:05:06 +0530

    changed category page ui

--- src/components/categoryrecipes.js
+++ src/components/categoryrecipes.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
-import './styles/allrecipes.css'
+import './styles/categoryrecipes.css'
 import axios from "axios";
-import RecipePage from "./recipepage";
-import { Link } from "react-router-dom";
+import RecipeModal from "./recipepage";
 
 function CategoryRecipes({ heading }) {
     const [categoryData, setCategoryData] = useState([]);
-    const [check, setCheck]=useState(null);
     useEffect(() => {
 
         axios.get(`https://api.edamam.com/api/recipes/v2?q=${heading}&type=public&beta=true&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&imageSize=REGULAR&random=true`)
@@ -17,6 +15,20 @@ function CategoryRecipes({ heading }) {
                 console.log(recipeWithImages.slice(0,6));
             })
     },[]);
+
+    const [selectedRecipe, setSelectedRecipe] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+
+
+    const openModal = (recipe) => {
+        setSelectedRecipe(recipe);
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setSelectedRecipe(null);
+        setShowModal(false);
+    };
 
     let settings = {
         // dots: false,
@@ -33,14 +45,14 @@ function CategoryRecipes({ heading }) {
             <h2>{heading}</h2>
             <Slider  {...settings}>
                 {categoryData.map((result) => (
-                    <div key={result.recipe.uri} className="slide" >
-                        <Link to="/recipe/${result.recipe.label}" state={{ recipeDetails:result.recipe}}>
+                    <div key={result.recipe.uri} className="slide" onClick={() => openModal(result.recipe)}>
                         <img src={result.recipe.image} alt={result.recipe.label} ></img>
-                        <p>{result.recipe.label}</p></Link>
+                        <p>{result.recipe.label}</p>
                     </div>
                 ))}
             </Slider>
-            {/* {selectedRecipe && <RecipePage recipeDetails={selectedRecipe}/>} */}
+            {selectedRecipe && (
+                <RecipeModal isOpen={showModal} onClose={closeModal} recipeDetails={selectedRecipe}/>)}
         </div>
         
     );
