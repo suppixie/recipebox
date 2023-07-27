80a6d18532667aa25da377e87f1896426044f32c
suppixie <mamthapatalay6@gmail.com>
2023-07-28 04:04:30 +0530

    changed homepage ui

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './styles/home.css';
+// import LoginPage from '../auth/login';
+// import SignUpPage from '../auth/signup';
+// import Recipe from '../auth/saverecipe';
 
 
 function Home(){
@@ -17,23 +20,23 @@ function Home(){
     return (<>
         <section>
             <div className='landing_page'>
-                <h1>Delicious Vegan Recipes</h1>
+                <div className='tagline'>
+                    <h1>Savor The Flavour in Every Recipe!</h1>
+                    <input className='search_bar' type='text' name='search_input' id="search_input" placeholder='Enter Ingredient/Dish'/>
+                </div>
+                <div className='animation'>
+                    <img></img>
+                </div>
             </div>
         </section>
-        <section className='search_and_random'>
-          <div>
-              <div className='search_bar'>
-                  <h2>Search for recipes</h2>
-                  <input type='text' name='search_input' id="search_input" placeholder='Enter Ingredient/Dish'/>
-              </div>
-
+        <section >
               <div className="specials" data-scroll data-scroll-speed="-0.6">
-                    <div className="textbox2">
+                    <div className="daily_specials_textarea">
                         <h1 >Daily<br/>Specials</h1>
                         <button className="Category-button" onClick="">View All Recipes</button>
                     </div>
 
-                    <div className="items">
+                    <div className="random_recipe_items">
                         {randomRecipes.map((randomrecipe) => (   
                                       <div key={randomrecipe.recipe.uri}>
                                           <img  src={randomrecipe.recipe.image} alt={randomrecipe.recipe.label}></img>
@@ -42,8 +45,6 @@ function Home(){
                                   ))}
                     </div> 
                 </div>
-                
-          </div>
         </section>
     </>
   )
