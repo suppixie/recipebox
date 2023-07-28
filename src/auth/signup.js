820aa7b9b73c9efe2e885598327993fc79aee02c
suppixie <mamthapatalay6@gmail.com>
2023-07-29 04:46:35 +0530

    Created Sign up page

--- src/auth/signup.js
+++ src/auth/signup.js
@@ -1,29 +1,34 @@
-import React from "react";
 import '../auth/signup.css';
-function SignUpPage(){
-    return(
-        <>
-        <div className="signup_page_background">
-        <div className="signup_block">
-            <h2>Login</h2>
-            <form action="/signup" method="post" className="signupForm"/>
-                <input type="text" name="username" placeholder="Username" required></input><br/>
-                <input type="email" name="email" placeholder="Email-id" required></input><br/>
-                <input type="password" name="password" placeholder="Password" required></input><br/>
-                <button type="submit">Sign Up</button><br/>
-            <p>Already have an account? <a href="/login.js">Login</a></p>
-        </div>
-        </div>
-        <script>
-            const response = JSON.parse(xhr.responseText);
-            if (response.message === 'Login successful!') {
-            alert(response.message);
-            window.location.href = '/home.js';
-            } else {
-            alert(response.message);
-            }
-        </script>
-  </>
-  )
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import ProfilePage from '../components/profile';
+
+const SignUpPage = () => {
+  const[username ,setUsername]=useState('');
+  const[email ,setEmail]=useState('');
+  const[password ,setPassword]=useState('');
+  const[cookies, setCookies]=useCookies(['user']);
+
+const handle= ()=>{
+  setCookies('Username', username,{path:'/'})
+  setCookies('EmailId', email,{path:'/'})
+  setCookies('Password', password,{path:'/'})
+  alert("Sign Up Successful");
+  window.location.href="/profile"
 }
+
+  return (
+    <div className="signup_page_background">
+      <h2>User Registration</h2>
+        <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required/>
+        <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required/>
+        <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
+        <button type="submit" onClick={handle}>Register</button>
+      <p>Already have an account? <a href="/login">Login</a></p>
+      {cookies && <ProfilePage cookieData={cookies} />}
+    </div>
+  );
+};
+
 export default SignUpPage;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { CookiesProvider } from "react-cookie";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
       <Router>
+          <CookiesProvider>
         <App />
+        </CookiesProvider>
       </Router>
+     
 );
 
 
