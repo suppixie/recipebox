f0c869b8475a44c88713a12a972fe5ece5b6913a
suppixie <mamthapatalay6@gmail.com>
2023-07-29 04:47:04 +0530

    Added Cookie data to profile page

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,14 +3,18 @@ import './styles/profile.css';
 
 
 
-function ProfilePage(){
+function ProfilePage(props){
+    const { Username, EmailId } = props.cookieData;
+    console.log(props.cookieData)
+
     
     return(
         <div className="profile_page">
             <div className="user_details">
                 <h1>Profile</h1>
-                <p className="pp">MP</p>
-                <h3>Mamtha Patalay</h3>
+                <p className="pp">(0v0)</p>
+                <h3>{Username}</h3>
+                <p>{EmailId}</p>
                 <p><b>Followers</b>:145</p>
                 <p><b>Following</b>:115</p>
             </div>
